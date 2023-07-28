7be7e95e7082e9f850a62d14866edeee4c4a570c
Szymcio587 <szymon.talar2882@gmail.com>
2023-07-28 19:43:01 +0200

    Creating a training data

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import train
 if __name__ == "__main__":
     sentence = "This is an example sentence."
     print(sentence)
-    train.LoadData()
+    train.InterpretData()
     print(words.Tokenize(sentence))
--- train.py
+++ train.py
@@ -1,6 +1,11 @@
 import json
 import words
 
+allWords = []
+tags = []
+xy = []
+XTrain = []
+YTrain = []
 
 def LoadData():
     with open('data.json', 'r') as f:
@@ -8,10 +13,8 @@ def LoadData():
     return data
 
 def InterpretData():
+    global tags
     data = LoadData()
-    allWords = []
-    tags = []
-    xy = []
     for response in data['responses']:
         tag = response['tag']
         tags.append(tag)
@@ -20,4 +23,23 @@ def InterpretData():
             allWords.extend(w)
             xy.append((w, tag))
 
-ignoreWords = ['?', '!', '.', ',']
+    ignoreWords = ['?', '!', '.', ',']
+    all_words = [words.Stem(w) for w in allWords if w not in ignoreWords]
+    all_words = sorted(set(all_words))
+    tags = sorted(set(tags))
+
+    #print(len(xy), "patterns")
+    #print(len(tags), "tags:", tags)
+    #print(len(all_words), "unique stemmed words:", all_words)
+
+    CreateTrainingData()
+
+def CreateTrainingData():
+    X_train = []
+    y_train = []
+    for (pattern_sentence, tag) in xy:
+        bag = words.BagOfWords(pattern_sentence, allWords)
+        X_train.append(bag)
+        label = tags.index(tag)
+        y_train.append(label)
+
